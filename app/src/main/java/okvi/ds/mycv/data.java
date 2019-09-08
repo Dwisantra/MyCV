970b6feabc02d1b5317fb1d0edeedb7e38b09665
Dwisantra <54524237+Dwisantra@users.noreply.github.com>
2019-09-08 15:12:01 +0700

    Initial commit

---
+++ app/src/main/java/okvi/ds/mycv/data.java
@@ -0,0 +1,15 @@
+package okvi.ds.mycv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+
+        getSupportActionBar().setTitle("Data Diri");
+    }
+}
